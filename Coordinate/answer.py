d97ec2c1996b97a8e6e67f8cabccf14f262c9374
SongeonLee <qkfka1040@icloud.com>
2022-12-07 11:49:31 +0900

    221207 프로그래머스 캐릭터좌표

--- Coordinate/answer.py
+++ Coordinate/answer.py
@@ -7,16 +7,16 @@
 # , right-> [1, 0]
 
 def solution(keyinput, board):
-    row = board[0]
-    col = board[1]
+    x_boundary = board[0] // 2
+    y_boundary = board[1] // 2
     result = [0,0]
     for i in keyinput:
-        if i == "left" and result[0]-1 >= -(row // 2):
+        if i == "left" and result[0]-1 >= -(x_boundary):
             result[0]-=1
-        elif i == "right" and result[0]+1 <= (row // 2):
+        elif i == "right" and result[0]+1 <= (x_boundary):
             result[0] += 1
-        elif i == "up" and result[1] +1 <= (col //2):
+        elif i == "up" and result[1] +1 <= (y_boundary):
             result[1] += 1
-        elif i == "down" and result[1] -1 >= -(col //2):
+        elif i == "down" and result[1] -1 >= -(y_boundary):
             result[1] -= 1
     return result
